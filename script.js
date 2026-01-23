eebf6a71268fac5156ea9212a7fd89ba14b8da9a
jfffh <157326539+jfffh@users.noreply.github.com>
2026-01-23 23:30:12 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+players = [
+    "villager",
+    "werewolf",
+]
+
+turn_order = [
+    "werewolf",
+    "seer",
+    "robber",
+    "troublemaker",
+    "insomniac"
+]
+
+current_player = 0
+current_turn = 0
+
+screen = "next_player"
+
+function get_next_player() {
+    document.getElementById("next_player_name").value = players[current_player]
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    init()
+})
+
+function init() {
+    show_current_screen()
+    if (screen == "next_player") {
+        get_next_player()
+    }
+}
+
+function show_current_screen() {
+    screens = document.querySelectorAll(".screen")
+    screens.forEach(element => {
+        element.classList.add("hidden")
+    });
+    document.getElementById(screen).classList.remove("hidden")
+}
+
+function start_turn() {
+    screen = "player_turn"
+    show_current_screen()
+}
